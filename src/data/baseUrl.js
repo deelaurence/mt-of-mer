3ed53018960a31ff79a710cada231aaf94047e9c
deelaurence <laurie143pelumi@gmail.com>
2024-06-05 23:27:22 +0100

    feat: admin

--- src/data/baseUrl.js
+++ src/data/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = "http://localhost:4000"
-// const baseUrl = "https://easy-erin-eel-sock.cyclic.app"
+// const baseUrl = "http://localhost:4000"
+const baseUrl = "https://uacc-api.onrender.com"
 export default baseUrl
